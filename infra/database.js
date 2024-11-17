50d5a653d80fabb7149da73017415d4fbb81a225
Mateus Farias <mateus.f2001@hotmail.com>
2024-11-17 00:25:24 +0000

    resolvido conflitos no workspace

--- infra/database.js
+++ infra/database.js
@@ -19,13 +19,23 @@ async function getNewClient() {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB, 
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSSLValues(), 
   });
 
   await client.connect();
   return client;
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default {
--- tests/integration/api/v1/orchestrator.js
+++ tests/integration/api/v1/orchestrator.js
@@ -1,16 +1,14 @@
 import retry from "async-retry";
+import database from "infra/database.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    return (
-      retry(fetchStatusPage),
-      {
-        retries: 100,
-        maxtimeout: 1000,
-      }
-    );
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
@@ -22,6 +20,13 @@ async function waitForAllServices() {
   }
 }
 
-export default {
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+const orchestrator = {
   waitForAllServices,
+  clearDatabase,
 };
+
+export default orchestrator;
