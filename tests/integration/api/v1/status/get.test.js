8728f68be20640eefe1d9f878f86626b3ebfd9fe
Mateus Farias <mateus.f2001@hotmail.com>
2024-11-18 18:59:11 +0000

    fix: corrige a versão do banco de dados

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -15,7 +15,7 @@ describe("GET /api/v1/status", () => {
       const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
       expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-      expect(responseBody.dependencies.database.version).toEqual("16.4");
+      expect(responseBody.dependencies.database.version).toEqual("16.5");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
     });
